7b7db526552f11ff15b2542cf9a1395cfb99c53e
Jrb <jrb.youssef@gmail.com>
2021-12-12 05:57:38 +0100

    Install Quill Editor

---
+++ src/components/Editor.vue
@@ -0,0 +1,37 @@
+<template>
+  <quill-editor
+    :content="value"
+    :options="editorOption"
+    @change="onEditorChange($event)"
+  />
+</template>
+
+<script>
+  import 'quill/dist/quill.core.css';
+  import 'quill/dist/quill.snow.css';
+  import { quillEditor } from 'vue-quill-editor';
+  
+  export default {
+    name: 'EditorComponent',
+
+    props: ['value'],
+  
+    components: { quillEditor },
+
+    data () {
+      return {
+        content: '',
+        editorOption: {
+          // Some Quill options...
+        }
+      }
+    },
+    methods: {
+      onEditorChange({ quill, html, text }) { // eslint-disable-line
+        this.content = html
+        this.$emit('input', this.content);
+        console.log('editor change!', this.content)
+      }
+    },
+  }
+</script>
---
+++ src/views/products/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <editor v-model="content" />
+    <div v-html="content" />
+  </div>
+</template>
+
+<script>
+  import Editor from '../../components/Editor.vue';
+
+  export default {
+    name: 'ProductIndex',
+
+    components: { Editor },
+
+    data () {
+      return {
+        content: '',
+      }
+    },
+  }
+</script>
